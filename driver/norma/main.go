76b076c1defa7bb62062ecafe9df55f098dc738b
Herbert Jordan <herbert@swrnd.ch>
2023-04-26 13:04:46 +0200

    Initiate driver binary and define a few small test scenarios

---
+++ driver/norma/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+// Run with `go run ./driver/norma`
+
+func main() {
+	app := &cli.App{
+		Name:      "Norma Network Runner",
+		HelpName:  "norma",
+		Usage:     "A set of tools for running network scenarios",
+		Copyright: "(c) 2023 Fantom Foundation",
+		Flags:     []cli.Flag{},
+		Commands: []*cli.Command{
+			&checkCommand,
+		},
+	}
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/Fantom-foundation/Norma
 go 1.20
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/golang/mock v1.6.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v2 v2.25.1 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
