0bcbdbdd6fe7d05d4abeaab42ff36bcf31ab85c7
Herbert Jordan <herbert@swrnd.ch>
2023-05-15 15:25:06 +0200

    Improve documentation of Metric subjects and value types

--- driver/monitoring/metrics.go
+++ driver/monitoring/metrics.go
@@ -7,6 +7,22 @@ import (
 // Metric defines a metric in the monitoring system. The type S is the type of
 // subject this metric is collected for (e.g., a node or the full network), and
 // the type T is the type of value produced by this metric.
+//
+// The subject of a metric is the object the metric's property is to be
+// associated to. There are two major subjects:
+//
+//   - monitoring.Network ... to be used for network-wide properties like the
+//     number of transactions in a block or the utilized gas. Those metrics are
+//     consistent throughout the network and do not require any finer
+//     granularity.
+//
+//   - monitoring.Node ... to be used for node-level properties like the time a
+//     block was completed, or the CPU usage at a givne time.
+//
+// Metric data is typically organized in data series, of which there are two
+// main types: monitoring.TimeSeries and monitoring.BlockSeries. The former is
+// associating a value to various points in time (using absolute time-stamps).
+// The latter associates a value to various block-numbers.
 type Metric[S any, T any] struct {
 	Name        string // used for unique identification of a metric
 	Description string // a description documenting the details of the metric
