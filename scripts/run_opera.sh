2868795cdb437b5fdd4bdb2b3622b2dbddb9846c
Kamil Jezek <kamil.jezek@gmail.com>
2023-06-15 18:12:02 +0200

    configures opera startup script to enable metrics (#79)

--- scripts/run_opera.sh
+++ scripts/run_opera.sh
@@ -10,4 +10,7 @@ echo "Opera is going to export its services on ${external_ip}"
     --http --http.addr 0.0.0.0 --http.port 18545 --http.api admin,eth,ftm \
     --ws --ws.addr 0.0.0.0 --ws.port 18546 --ws.api admin,eth,ftm \
     --pprof --pprof.addr 0.0.0.0 \
-    --nat=extip:${external_ip}
+    --nat=extip:${external_ip} \
+    --metrics \
+    --metrics.expensive
+
