910805bf72f84aa2554336a815698036d3cdd09f
mike <pelanek.lukas@seznam.cz>
2023-06-23 14:22:05 +0200

    Add linear shaper

--- load/shaper/linear.go
+++ load/shaper/linear.go
@@ -4,27 +4,36 @@ import "time"
 
 // LinearShaper is used to send txs with a linearly increasing frequency
 type LinearShaper struct {
-	interval  time.Duration
-	increment time.Duration
-	lastTime  time.Time
+	interval           time.Duration
+	currentFrequency   float32
+	incrementFrequency float32
+	currentTick        time.Duration
 }
 
 func NewLinearShaper(startFrequency, incrementFrequency float32) *LinearShaper {
+	if startFrequency <= 0 {
+		startFrequency = 1
+	}
 	return &LinearShaper{
-		interval:  time.Duration(float32(time.Second) / startFrequency),
-		increment: time.Duration(float32(time.Second) / incrementFrequency),
-		lastTime:  time.Now(),
+		currentFrequency:   startFrequency,
+		interval:           time.Duration(float32(time.Second) / startFrequency),
+		incrementFrequency: incrementFrequency,
+		currentTick:        0,
 	}
 }
 
+// GetNextWaitTime returns the next wait time based on the current frequency
+// and the increment frequency.
 func (s *LinearShaper) GetNextWaitTime() time.Duration {
-	currentTime := time.Now()
-	elapsedTime := currentTime.Sub(s.lastTime)
-
-	waitTime := s.interval - elapsedTime
-
-	s.interval += s.increment
-	s.lastTime = currentTime
-
-	return waitTime
+	// Increase the current frequency if the current tick is greater than or
+	// equal to one second. That means that the current frequency is completed.
+	if s.currentTick >= time.Second {
+		s.currentFrequency += s.incrementFrequency
+		// Round the interval to the nearest microsecond.
+		s.interval = time.Duration(float32(time.Second) / s.currentFrequency).Round(time.Microsecond)
+		s.currentTick = 0
+	}
+	// Increase the current tick by the interval.
+	s.currentTick += s.interval
+	return s.interval
 }
--- load/shaper/linear_test.go
+++ load/shaper/linear_test.go
@@ -6,18 +6,42 @@ import (
 )
 
 func TestLinearShaper(t *testing.T) {
-	// Create a LinearShaper with a start frequency of 1 Hz and increment of 0.5 Hz
-	shaper := NewLinearShaper(1.0, 0.5)
+	// Create a LinearShaper with a start frequency of 5 Hz and increment of 5 Hz
+	shaper := NewLinearShaper(5, 5)
 
-	expectedIntervals := []time.Duration{
-		time.Duration(float32(time.Second) / 1.0),
-		time.Duration(float32(time.Second) / 1.5),
-		time.Duration(float32(time.Second) / 2.0),
+	// Starting frequency of 5 Hz means that the interval is 200 ms
+	// So we expect to return 200 ms for the first 5 calls
+	for i := 0; i < 5; i++ {
+		waitTime := shaper.GetNextWaitTime()
+		if waitTime != 200*time.Millisecond {
+			t.Errorf("Expected 200 ms, got %v", waitTime)
+		}
 	}
 
+	// The next call should increase the frequency to 10 Hz
+	// So we expect to return 100 ms for the next 10 calls
 	for i := 0; i < 10; i++ {
-		t.Errorf("Expected %d, got %d", expectedIntervals[i], shaper.GetNextWaitTime())
+		waitTime := shaper.GetNextWaitTime()
+		if waitTime != 100*time.Millisecond {
+			t.Errorf("Expected 100 ms, got %v", waitTime)
+		}
+	}
 
-		time.Sleep(shaper.GetNextWaitTime())
+	// The next call should increase the frequency to 15 Hz
+	// So we expect to return 66.67 ms for the next 15 calls
+	for i := 0; i < 15; i++ {
+		waitTime := shaper.GetNextWaitTime()
+		if waitTime != 66*time.Millisecond+667*time.Microsecond {
+			t.Errorf("Expected 66.67 ms, got %v", waitTime)
+		}
+	}
+
+	// The next call should increase the frequency to 20 Hz
+	// So we expect to return 50 ms for the next 20 calls
+	for i := 0; i < 20; i++ {
+		waitTime := shaper.GetNextWaitTime()
+		if waitTime != 50*time.Millisecond {
+			t.Errorf("Expected 50 ms, got %v", waitTime)
+		}
 	}
 }
