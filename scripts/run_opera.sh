e55ced7b1bb03cf22bab3c527e8ccd0da14e0d95
Jan Kalina <jan.kalina@fantom.foundation>
2023-06-28 14:35:07 +0200

    Use new Opera fakenetgaspower parameter to increase tx throughput (#106)

--- scripts/run_opera.sh
+++ scripts/run_opera.sh
@@ -7,6 +7,7 @@ echo "Opera is going to export its services on ${external_ip}"
 
 # Starting opera as part of a fake net with RPC service.
 ./opera --fakenet ${VALIDATOR_NUMBER}/${VALIDATORS_COUNT} --statedb.impl=${STATE_DB_IMPL} \
+    --fakenetgaspower 1000 \
     --http --http.addr 0.0.0.0 --http.port 18545 --http.api admin,eth,ftm \
     --ws --ws.addr 0.0.0.0 --ws.port 18546 --ws.api admin,eth,ftm \
     --pprof --pprof.addr 0.0.0.0 \
