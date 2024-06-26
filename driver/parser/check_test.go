4447c755b9db3002f00ef2ac601fe9053cdf73f0
rapol-fantom <rapol@fantom.foundation>
2024-06-26 13:42:31 +0000

    removed check related to node alive

--- driver/parser/check_test.go
+++ driver/parser/check_test.go
@@ -506,38 +506,4 @@ func TestScenario_NodeGenesisIssuesAreDetected(t *testing.T) {
 		t.Errorf("targeted file is not a genesis but issue was not detected")
 	}
 
-	*start = 70
-	scenario = Scenario{
-		Name:     "Test",
-		Duration: 60,
-		Nodes: []Node{
-			{Genesis: Genesis{Import: &GenesisTarget{
-				Start: start,
-				Path:  "/does/exist.g",
-			}}},
-		},
-	}
-	if err := scenario.Check(); err == nil || !strings.Contains(err.Error(), "event start must be <= node end") {
-		t.Errorf("genesis import/export outside duration but issue was not detected")
-	}
-
-}
-
-func TestScenario_NodeEventIssuesAreDetected(t *testing.T) {
-	start := new(float32)
-
-	*start = 70
-	scenario := Scenario{
-		Name:     "Test",
-		Duration: 60,
-		Nodes: []Node{
-			{Event: Event{Import: &EventTarget{
-				Start: start,
-				Path:  "/path/to/event",
-			}}},
-		},
-	}
-	if err := scenario.Check(); err == nil || !strings.Contains(err.Error(), "event start must be <= node end") {
-		t.Errorf("event timing is wrong but issue was not detected")
-	}
 }
