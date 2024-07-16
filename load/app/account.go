1bdd1b9db6d59618566b4d88acd6f4a5fd875cf7
mm <mm@fantom.foundation>
2024-07-16 18:01:08 +0200

    Fix imports

--- load/app/account.go
+++ load/app/account.go
@@ -24,7 +24,10 @@ import (
 	"sync/atomic"
 
 	"github.com/Fantom-foundation/Norma/driver/rpc"
+	contract "github.com/Fantom-foundation/Norma/load/contracts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
