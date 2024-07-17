465efd9fbe8dd9bb52ee065a01beaaaaf9291631
RT <rapol@fantom.foundation>
2024-07-17 07:20:59 +0000

    network mock now import from github rather than go.uber

--- driver/network_mock.go
+++ driver/network_mock.go
@@ -14,7 +14,7 @@ import (
 
 	rpc "github.com/Fantom-foundation/Norma/driver/rpc"
 	types "github.com/ethereum/go-ethereum/core/types"
-	gomock "go.uber.org/mock/gomock"
+	gomock "github.com/golang/mock/gomock"
 )
 
 // MockNetwork is a mock of Network interface.
