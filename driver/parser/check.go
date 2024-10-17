8dd02f7e1df8d4fd790eaec39c1be01aa91316c3
RT <rapol@fantom.foundation>
2024-10-17 06:59:06 +0000

    remove check for num_validator

--- driver/parser/check.go
+++ driver/parser/check.go
@@ -41,9 +41,6 @@ func (s *Scenario) Check() error {
 	if s.Duration <= 0 {
 		errs = append(errs, fmt.Errorf("scenario duration must be > 0"))
 	}
-	if s.NumValidators != nil && *s.NumValidators != 0 {
-		errs = append(errs, fmt.Errorf("scenario contains deprecated expression NumValidator"))
-	}
 	if err := s.checkValidatorConstraints(); err != nil {
 		errs = append(errs, err)
 	}
