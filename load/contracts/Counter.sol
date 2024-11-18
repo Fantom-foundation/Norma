44532b7cb303a414a752378a87523fa8c17fb816
Herbert Jordan <herbert@swrnd.ch>
2024-11-18 14:32:03 +0100

    Make gas costs of counter increments uniform

--- load/contracts/Counter.sol
+++ load/contracts/Counter.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.4;
 
 contract Counter {
-    int private count = 0;
+    // counter is an internal counter tracking the number of increment calls.
+    // The counter is initialized to 1 to make all increment-counter calls
+    // equally expensive. Otherwise, the first call incrementing the counter
+    // from 0 to 1 would have to pay extra gas for the storage allocation.
+    int private count = 1;
 
     function incrementCounter() public {
         count += 1;
     }
 
-    function decrementCounter() public {
-        count -= 1;
-    }
-
     function getCount() public view returns (int) {
-        return count;
+        return count-1;
     }
 }
