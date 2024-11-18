6b0c57d8b9f0e23e8326737b90b2b7ad9e3ff909
Kamil Jezek <kamil.jezek@gmail.com>
2024-11-18 08:34:17 +0100

    fixes log to reflect active nodes (#288)

--- driver/norma/run.go
+++ driver/norma/run.go
@@ -19,10 +19,8 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 	"path/filepath"
-	"sort"
 	"time"
 
 	"github.com/Fantom-foundation/Norma/driver/checking"
@@ -32,14 +30,11 @@ import (
 	"github.com/Fantom-foundation/Norma/driver/executor"
 	"github.com/Fantom-foundation/Norma/driver/monitoring"
 	_ "github.com/Fantom-foundation/Norma/driver/monitoring/app"
-	netmon "github.com/Fantom-foundation/Norma/driver/monitoring/network"
-	nodemon "github.com/Fantom-foundation/Norma/driver/monitoring/node"
 	prometheusmon "github.com/Fantom-foundation/Norma/driver/monitoring/prometheus"
 	_ "github.com/Fantom-foundation/Norma/driver/monitoring/user"
 	"github.com/Fantom-foundation/Norma/driver/network/local"
 	"github.com/Fantom-foundation/Norma/driver/parser"
 	"github.com/urfave/cli/v2"
-	"golang.org/x/exp/constraints"
 )
 
 // Run with `go run ./driver/norma run <scenario.yml>`
@@ -215,7 +210,7 @@ func run(ctx *cli.Context) (err error) {
 
 	// Run scenario.
 	fmt.Printf("Running '%s' ...\n", path)
-	logger := startProgressLogger(monitor)
+	logger := startProgressLogger(monitor, net)
 	defer logger.shutdown()
 	err = executor.Run(clock, net, &scenario, outputDir)
 	if err != nil {
@@ -235,104 +230,4 @@ func run(ctx *cli.Context) (err error) {
 	}
 
 	return nil
-}
-
-type progressLogger struct {
-	monitor *monitoring.Monitor
-	stop    chan<- bool
-	done    <-chan bool
-}
-
-func startProgressLogger(monitor *monitoring.Monitor) *progressLogger {
-	stop := make(chan bool)
-	done := make(chan bool)
-
-	go func() {
-		defer close(done)
-		ticker := time.NewTicker(time.Second)
-		for {
-			select {
-			case <-stop:
-				return
-			case <-ticker.C:
-				logState(monitor)
-			}
-		}
-	}()
-
-	return &progressLogger{
-		monitor,
-		stop,
-		done,
-	}
-}
-
-func (l *progressLogger) shutdown() {
-	close(l.stop)
-	<-l.done
-}
-
-func logState(monitor *monitoring.Monitor) {
-	numNodes := getNumNodes(monitor)
-	blockStatuses := getBlockStatuses(monitor)
-	txPers := getTxPerSec(monitor)
-	txs := getNumTxs(monitor)
-	gas := getGasUsed(monitor)
-	processingTimes := getBlockProcessingTimes(monitor)
-
-	log.Printf("Nodes: %s, block heights: %v, tx/s: %v, txs: %v, gas: %s, block processing: %v", numNodes, blockStatuses, txPers, txs, gas, processingTimes)
-}
-
-func getNumNodes(monitor *monitoring.Monitor) string {
-	data, exists := monitoring.GetData(monitor, monitoring.Network{}, netmon.NumberOfNodes)
-	return getLastValAsString[monitoring.Time, int](exists, data)
-}
-
-func getNumTxs(monitor *monitoring.Monitor) string {
-	data, exists := monitoring.GetData(monitor, monitoring.Network{}, netmon.BlockNumberOfTransactions)
-	return getLastValAsString[monitoring.BlockNumber, int](exists, data)
-}
-
-func getTxPerSec(monitor *monitoring.Monitor) []string {
-	metric := nodemon.TransactionsThroughput
-	return getLastValAllSubjects[monitoring.BlockNumber, float32](monitor, metric)
-}
-
-func getGasUsed(monitor *monitoring.Monitor) string {
-	data, exists := monitoring.GetData(monitor, monitoring.Network{}, netmon.BlockGasUsed)
-	return getLastValAsString[monitoring.BlockNumber, int](exists, data)
-}
-
-func getBlockStatuses(monitor *monitoring.Monitor) []string {
-	metric := nodemon.NodeBlockStatus
-	return getLastValAllSubjects[monitoring.Time, monitoring.BlockStatus, monitoring.Series[monitoring.Time, monitoring.BlockStatus]](monitor, metric)
-}
-
-func getBlockProcessingTimes(monitor *monitoring.Monitor) []string {
-	metric := nodemon.BlockEventAndTxsProcessingTime
-	return getLastValAllSubjects[monitoring.BlockNumber, time.Duration, monitoring.Series[monitoring.BlockNumber, time.Duration]](monitor, metric)
-}
-
-func getLastValAllSubjects[K constraints.Ordered, T any, X monitoring.Series[K, T]](monitor *monitoring.Monitor, metric monitoring.Metric[monitoring.Node, X]) []string {
-	nodes := monitoring.GetSubjects(monitor, metric)
-	sort.Slice(nodes, func(i, j int) bool { return nodes[i] < nodes[j] })
-
-	res := make([]string, 0, len(nodes))
-	for _, node := range nodes {
-		data, exists := monitoring.GetData(monitor, node, metric)
-		var d string = getLastValAsString[K, T](exists, data)
-		res = append(res, d)
-	}
-	return res
-}
-
-func getLastValAsString[K constraints.Ordered, T any](exists bool, series monitoring.Series[K, T]) string {
-	if !exists || series == nil {
-		return "N/A"
-	}
-	point := series.GetLatest()
-	if point == nil {
-		return "N/A"
-	}
-	return fmt.Sprintf("%v", point.Value)
 }
